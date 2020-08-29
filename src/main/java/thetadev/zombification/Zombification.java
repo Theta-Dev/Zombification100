3d5595b6c6bab113674f93fbda76d5136829850c
Theta-Dev <t.testboy@gmail.com>
2020-08-29 21:06:14 +0200

    Made the mod serverside-only

--- src/main/java/thetadev/zombification/Zombification.java
+++ src/main/java/thetadev/zombification/Zombification.java
@@ -11,7 +11,11 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,8 +27,8 @@ public class Zombification
 
     public Zombification() {
         MinecraftForge.EVENT_BUS.register(this);
-
         ConfigHandler.register();
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
     @SubscribeEvent
