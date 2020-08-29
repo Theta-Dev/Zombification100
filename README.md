7c98537aeb1c27a86b3db994ba27c9368aaeb68b
Theta-Dev <t.testboy@gmail.com>
2020-08-29 21:06:14 +0200

    Made the mod serverside-only

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Set the chance of a villager being zombified independent from the game difficult
 Also allows you to set the chance of a zombified villager to be able to pick up loot and to be persistent.
 
 All chances are set to 100% by default, change them in the config.
+
+This mod is only required serverside, so you can put it on your server and play with a vanilla client.
--- src/main/java/thetadev/zombification/Zombification.java
+++ src/main/java/thetadev/zombification/Zombification.java
@@ -12,7 +12,11 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,8 +28,8 @@ public class Zombification
 
     public Zombification() {
         MinecraftForge.EVENT_BUS.register(this);
-
         ConfigHandler.register();
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
     @SubscribeEvent
